93e2dc4d01cc5c6d74f5cdc6594147dfe9b00598
tasneya2025 <repha5505urbi@gmail.com>
2026-02-19 21:48:41 +0600

    error message show added

--- View/login/login.php
+++ View/login/login.php
@@ -1,6 +1,8 @@
-<?php session_start(); ?>
+<?php 
+session_start(); 
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Login | SHOPPON</title>
@@ -20,27 +22,43 @@
         <div class="form-panel">
             <form action="../../Controller/loginController.php" method="POST">
                 <h2 id="logTag">Login to Dashboard</h2><br>
-                    <select name="usertype" id="usertype">
-                    <option id="none" value="">Select your role</option>
-                    <option id="seller" value="seller">Seller</option>
-                    <option id="buyer" value="buyer">Buyer</option>
-                    <option id="admin" value="admin">Admin</option>
-                </select><br><br>
+
+                <?php if(isset($_SESSION['genErr'])): ?>
+                    <span class="error-text"><?php echo $_SESSION['genErr']; unset($_SESSION['genErr']); ?></span>
+                <?php endif; ?>
+
+                <div class="select-group">
+                    <select name="usertype" id="usertype" required>
+                        <option id="none" value="">Select your role</option>
+                        <option id="seller" value="seller">Seller</option>
+                        <option id="buyer" value="buyer">Buyer</option>
+                        <option id="admin" value="admin">Admin</option>
+                    </select>
+                    <?php if(isset($_SESSION['userErr'])): ?>
+                        <span class="error-text"><?php echo $_SESSION['userErr']; unset($_SESSION['userErr']); ?></span>
+                    <?php endif; ?>
+                </div>
                  
                 <div class="input-group">
-                    <input type="email" id="email" name="email" required placeholder=" ">
+                    <input type="text" id="email" name="email" required placeholder=" ">
                     <label for="email">Enter email</label>
                 </div>
+                <?php if(isset($_SESSION['emailErr'])): ?>
+                    <span class="error-text"><?php echo $_SESSION['emailErr']; unset($_SESSION['emailErr']); ?></span>
+                <?php endif; ?>
 
                 <div class="input-group">
                     <input type="password" id="password" name="password" required placeholder=" ">
                     <label for="password">Enter password</label>
                     <i class="fa-solid fa-eye" id="togglePassword"></i>
                 </div>
+                <?php if(isset($_SESSION['passErr'])): ?>
+                    <span class="error-text"><?php echo $_SESSION['passErr']; unset($_SESSION['passErr']); ?></span>
+                <?php endif; ?>
 
                 <div class="auth-options">
                     <label class="remember-me">
-                    <input type="checkbox" name="remember"> Remember me
+                        <input type="checkbox" name="remember"> Remember me
                     </label>
                     <a href="#" class="forgot-pass">Forgot Password?</a>
                 </div>
