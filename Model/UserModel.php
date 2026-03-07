04fc79aad4f45caa8cce069cb5b95545b7b81df9
tasneya2025 <repha5505urbi@gmail.com>
2026-03-07 21:09:05 +0600

    user model updated

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -3,8 +3,6 @@ require_once("db.php");
 
 function authuser($email, $password, $roleId) {
     $conn = get_db_connection();
-    
-
     $sql = "SELECT * FROM seller WHERE email='$email' AND role='$roleId'";
     $result = mysqli_query($conn, $sql);
 
@@ -14,7 +12,41 @@ function authuser($email, $password, $roleId) {
         if (password_verify($password, $user['password'])) {
             return $user; 
         }
+
+        else {
+            return "WRONG_PASSWORD";
+        }
+
     }
-    return false;
+    return "EMAIL_NOT_FOUND";
+
 }
+
+
+function getUserDataByEmail($email) {
+    $conn = get_db_connection();
+    $sql = "SELECT * FROM seller WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc();
+}
+
+function updateUserInfo($email, $fullname, $address) {
+    $conn = get_db_connection();
+    $sql = "UPDATE seller SET fullname=?, address=? WHERE email=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $fullname, $address, $email);
+    return $stmt->execute();
+}
+
+function updatePasswordInDb($email, $hashed_password) {
+    $conn = get_db_connection();
+    $sql = "UPDATE seller SET password=? WHERE email=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $hashed_password, $email);
+    return $stmt->execute();
+}
+
+
 ?>
